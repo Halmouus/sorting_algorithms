0289ef2e6d373dffb503ad491997ae4d95430504
Halmouus <javierminimaz@gmail.com>
2023-07-24 22:03:44 +0100

    fixing bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -22,6 +22,8 @@ c = *a;
 void bubble_sort(int *array, size_t size)
 {
 unsigned long int i, j, swap = 1;
+if (!array || size < 2)
+return;
 for (i = 0; i < size - 1; i++)
 {
 swap = 0;
@@ -31,8 +33,8 @@ if (array[j] > array[j + 1])
 {
 swap_int(&array[j], &array[j + 1]);
 swap = 1;
-}
 print_array(array, size);
+}
 }
 if (swap == 0)
 {
