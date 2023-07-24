bef8ed92fa3e88849d8d5a3be10cb60a3d4faa3f
Halmouus <javierminimaz@gmail.com>
2023-07-24 21:44:11 +0100

    revert back

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,24 +8,22 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, len = size;
-	int swap = 0;
-
-	if (array == NULL || size < 2)
-		return;
-
-	while (swap == 0)
-	{
-		swap = 1;
-		for (i = 0; i < len - 1; i++)
-		{
-			if (array[i] > array[i + 1])
-			{
-				swap_int(array + i, array + i + 1);
-				print_array(array, size);
-				swap = 0;
-			}
-		}
-		len--;
-	}
+unsigned long int i, j, swap = 1;
+for (i = 0; i < size - 1; i++)
+{
+swap = 0;
+for (j = 0; j < size - i - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+swap_int(&array[j], &array[j + 1]);
+swap = 1;
+}
+print_array(array, size);
+}
+if (swap == 0)
+{
+break;
+}
+}
 }
