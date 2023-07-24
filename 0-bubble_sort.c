e7d8533783a17e469295a1ca2d7d9ef8689715e8
Halmouus <javierminimaz@gmail.com>
2023-07-24 21:54:12 +0100

    adding swap_int at each C file

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,19 @@
 #include "sort.h"
 
 /**
+ * swap_int - function that swaps the values of two integers
+ * @a: pointer to the first integer to be swapped
+ * @b: pointer to the second integer to be swapped
+ */
+void swap_int(int *a, int *b)
+{
+int c;
+c = *a;
+*a = *b;
+*b = c;
+}
+
+/**
  * bubble_sort - sorts an array of integers.
  * @array: array of integers to sort.
  * @size: size of the array.
